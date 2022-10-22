aa66864e93466f7bd92999f801c9fce14740192b
今天也很困 <105327571+like-monday@users.noreply.github.com>
2022-10-22 19:50:15 +0800

    更新

--- dx_v/src/main.js
+++ dx_v/src/main.js
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
-import  router from './router/index.js';
+import { initRouter } from './router'
 
 const app = createApp(App)
-app.use(router)
+initRouter(app)
+app.use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 app.mount('#app')
