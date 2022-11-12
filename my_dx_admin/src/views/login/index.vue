7dc03377edee6c518f1fc23ef448809a264c2d29
今天也很困 <105327571+like-monday@users.noreply.github.com>
2022-11-12 20:18:04 +0800

    功能全部完成

--- my_dx_admin/src/views/login/index.vue
+++ my_dx_admin/src/views/login/index.vue
@@ -1,259 +1,306 @@
 <template>
-  <div class="login-container">
-    <el-form
-      ref="loginForm"
-      :model="loginForm"
-      :rules="loginRules"
-      class="login-form"
-      auto-complete="on"
-      label-position="left"
-    >
-      <div class="title-container">
-        <h3 class="title">登录</h3>
-      </div>
-
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
-      </el-form-item>
-
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="Password"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon
-            :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
+  <div class="login">
+    <div class="from">
+      <div class="title">SIGNIN FORM</div>
+      <div class="inputs">
+        <div class="form-control">
+          <input
+            ref="account"
+            type="value"
+            v-model="fromdata.account"
+            required=""
           />
-        </span>
-      </el-form-item>
+          <label>
+            <span style="transition-delay: 0ms">U</span
+            ><span style="transition-delay: 50ms">s</span
+            ><span style="transition-delay: 100ms">e</span
+            ><span style="transition-delay: 150ms">r</span
+            ><span style="transition-delay: 200ms">n</span>
+            <span style="transition-delay: 250ms">a</span>
+            <span style="transition-delay: 300ms">m</span>
+            <span style="transition-delay: 350ms">e</span></label
+          >
+        </div>
 
-      <el-button
-        :loading="loading"
-        type="primary"
-        style="width: 100%; margin-bottom: 30px"
-        @click.native.prevent="handleLogin"
-        >Login</el-button
-      >
-
-      <div class="tips">
-        <span style="margin-right: 20px">username: admin</span>
-        <span> password: any</span>
+        <div class="form-password">
+          <input
+            ref="password"
+            type="value"
+            v-model="fromdata.password"
+            required=""
+          />
+          <label>
+            <span style="transition-delay: 0ms">P</span
+            ><span style="transition-delay: 50ms">a</span
+            ><span style="transition-delay: 100ms">s</span
+            ><span style="transition-delay: 150ms">s</span
+            ><span style="transition-delay: 200ms">w</span>
+            <span style="transition-delay: 250ms">o</span>
+            <span style="transition-delay: 300ms">r</span>
+            <span style="transition-delay: 350ms">d</span></label
+          >
+        </div>
       </div>
-    </el-form>
+      <div class="remember">
+        <label class="container">
+          <input type="checkbox" v-model="checked" @change="Checkbox" />
+          <div class="checkmark"></div>
+        </label>
+        <span class="rm">Remember me</span>
+        <span class="fp">Forgot password?</span>
+      </div>
+      <div class="bt">
+        <div class="left"></div>
+        <div class="right"></div>
+        <div class="login_bt" @click="Login">SIGN IN</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { validUsername } from "@/utils/validate";
-
 export default {
-  name: "Login",
-  data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error("Please enter the correct user name"));
-      } else {
-        callback();
-      }
-    };
-    const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error("The password can not be less than 6 digits"));
-      } else {
-        callback();
-      }
-    };
+  name: 'Login',
+  data () {
     return {
-      loginForm: {
-        username: "admin",
-        password: "111111",
-      },
-      loginRules: {
-        username: [
-          { required: true, trigger: "blur", validator: validateUsername },
-        ],
-        password: [
-          { required: true, trigger: "blur", validator: validatePassword },
-        ],
-      },
-      loading: false,
-      passwordType: "password",
-      redirect: undefined,
-    };
-  },
-  watch: {
-    $route: {
-      handler: function (route) {
-        this.redirect = route.query && route.query.redirect;
-      },
-      immediate: true,
-    },
+      checked: false,
+      fromdata: {
+        account: '',
+        password: ''
+      }
+    }
   },
   methods: {
-    showPwd() {
-      if (this.passwordType === "password") {
-        this.passwordType = "";
-      } else {
-        this.passwordType = "password";
-      }
-      this.$nextTick(() => {
-        this.$refs.password.focus();
-      });
+    // 登录
+    Login () {
+      console.log('点击了登录按钮');
+      console.log('当前登录账号:', this.fromdata.account);
+      console.log('当前登录密码:', this.fromdata.password);
     },
-    handleLogin() {
-      this.$refs.loginForm.validate((valid) => {
-        if (valid) {
-          this.loading = true;
-          this.$store
-            .dispatch("user/login", this.loginForm)
-            .then(() => {
-              sessionStorage.setItem('account',this.loginForm.username)
-              sessionStorage.setItem('password',this.loginForm.password)
-              this.$router.push({ path: this.redirect || "/" });
-              this.loading = false;
-            })
-            .catch(() => {
-              this.loading = false;
-            });
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
-    },
-  },
-};
+    // 勾选记住密码
+    Checkbox () {
+      console.log(this.checked);
+    }
+  }
+}
 </script>
 
-<style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
-
-$bg: #283443;
-$light_gray: #fff;
-$cursor: #fff;
-
-@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-  .login-container .el-input input {
-    color: $cursor;
-  }
-}
-
-/* reset element-ui css */
-.login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
-      caret-color: $cursor;
-
-      &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
-      }
-    }
-  }
-
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
-  }
-}
-</style>
-
-<style lang="scss" scoped>
-$bg: #2d3a4b;
-$dark_gray: #889aa4;
-$light_gray: #eee;
-
-.login-container {
-  min-height: 100%;
-  width: 100%;
-  background-color: $bg;
+<style scoped lang="less">
+.login {
+  width: 100vw;
+  height: 100vh;
+  background-color: #142335;
   overflow: hidden;
+  .from {
+    // background: rgba(255, 255, 255, 0.1);
+    // -webkit-backdrop-filter: blur(8px);
+    // backdrop-filter: blur(8px);
+    // border-radius: 25px;
+    // box-shadow: inset 0 0 6px rgba(255, 255, 255, 0.2);
+    background: rgba(255, 255, 255, 0.1);
 
-  .login-form {
     position: relative;
-    width: 520px;
-    max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
-    overflow: hidden;
-  }
-
-  .tips {
-    font-size: 14px;
-    color: #fff;
-    margin-bottom: 10px;
-
-    span {
-      &:first-of-type {
-        margin-right: 16px;
-      }
-    }
-  }
-
-  .svg-container {
-    padding: 6px 5px 6px 15px;
-    color: $dark_gray;
-    vertical-align: middle;
-    width: 30px;
-    display: inline-block;
-  }
-
-  .title-container {
-    position: relative;
+    margin: 200px auto;
+    width: 500px;
+    height: 400px;
 
     .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
+      width: 100%;
+      height: 50px;
+      background-color: #02a299;
+      color: #fff;
+      line-height: 50px;
       text-align: center;
-      font-weight: bold;
+      letter-spacing: 8px;
     }
-  }
 
-  .show-pwd {
-    position: absolute;
-    right: 10px;
-    top: 7px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
+    .inputs {
+      .form-control {
+        position: relative;
+        top: 40px;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 70%;
+      }
+
+      .form-password {
+        position: relative;
+        top: 80px;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 70%;
+      }
+
+      input:focus,
+      input:valid {
+        outline: 0;
+        border-bottom-color: lightblue;
+      }
+
+      label {
+        position: absolute;
+        top: 15px;
+        left: 0;
+        pointer-events: none;
+      }
+
+      label span {
+        display: inline-block;
+        font-size: 18px;
+        min-width: 5px;
+        color: #fff;
+        transition: 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+      }
+
+      input:focus + label span,
+      input:valid + label span {
+        color: lightblue;
+        transform: translateY(-30px);
+      }
+
+      input {
+        background-color: transparent;
+        border: 0;
+        border-bottom: 2px #fff solid;
+        display: block;
+        width: 100%;
+        padding: 15px 0;
+        font-size: 18px;
+        color: #fff;
+      }
+    }
+
+    .remember {
+      position: relative;
+      top: 120px;
+      left: 10%;
+
+      .rm {
+        position: absolute;
+        top: 2px;
+        left: 30px;
+        color: #fff;
+      }
+
+      .fp {
+        position: absolute;
+        cursor: pointer;
+        top: 2px;
+        left: 280px;
+        color: #fff;
+      }
+      /* Hide the default checkbox */
+      .container input {
+        position: absolute;
+        opacity: 0;
+        cursor: pointer;
+        height: 0;
+        width: 0;
+      }
+
+      .container {
+        display: inline-block;
+        position: relative;
+        cursor: pointer;
+        font-size: 20px;
+        user-select: none;
+      }
+
+      /* Create a custom checkbox */
+      .checkmark {
+        position: relative;
+        top: 0;
+        left: 0;
+        height: 20px;
+        width: 20px;
+        background-color: #ccc;
+        transition: all 0.3s;
+        border-radius: 5px;
+      }
+
+      /* When the checkbox is checked, add a blue background */
+      .container input:checked ~ .checkmark {
+        background-color: #47da99;
+        animation: pop 0.5s;
+        animation-direction: alternate;
+      }
+
+      /* Create the checkmark/indicator (hidden when not checked) */
+      .checkmark:after {
+        content: "";
+        position: absolute;
+        display: none;
+      }
+
+      /* Show the checkmark when checked */
+      .container input:checked ~ .checkmark:after {
+        display: block;
+      }
+
+      /* Style the checkmark/indicator */
+      .container .checkmark:after {
+        left: 7px;
+        top: 2px;
+        width: 5px;
+        height: 10px;
+        border: solid white;
+        border-width: 0 0.15em 0.15em 0;
+        transform: rotate(45deg);
+      }
+
+      @keyframes pop {
+        0% {
+          transform: scale(1);
+        }
+
+        50% {
+          transform: scale(0.9);
+        }
+
+        100% {
+          transform: scale(1);
+        }
+      }
+    }
+
+    .bt {
+      position: absolute;
+      left: 0;
+      bottom: -10px;
+      width: 108%;
+      height: 60px;
+      background-color: #fff;
+      left: 50%;
+      transform: translateX(-50%);
+      text-align: center;
+      line-height: 60px;
+      cursor: pointer;
+
+      .left {
+        position: absolute;
+        top: -20px;
+        left: 0;
+        width: 0;
+        height: 0;
+        border-bottom: 20px solid rgb(255, 255, 255);
+        border-left: 20px solid transparent;
+      }
+
+      .right {
+        position: absolute;
+        top: -20px;
+        right: 0;
+        width: 0;
+        height: 0;
+        border-bottom: 20px solid rgb(255, 255, 255);
+        border-right: 20px solid transparent;
+      }
+
+      .login_bt {
+        font-size: 20px;
+        color: #02a299;
+      }
+    }
   }
 }
 </style>
