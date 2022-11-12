640deb3d5092217d630dc31c3c18d1dbd0b475d0
今天也很困 <105327571+like-monday@users.noreply.github.com>
2022-11-12 17:13:57 +0800

    功能全部完整

--- my_dx_admin/src/main.js
+++ my_dx_admin/src/main.js
@@ -6,11 +6,15 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './style.css'
 
+// 注册全局方法
+import formateDate from '@/utils/formateDate'
+
 Vue.use(ElementUI)
 Vue.config.productionTip = false
+Vue.prototype.$formateDate = formateDate
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
