e5c831a26b5c77ddfecfeec708e8ff254bf5941d
今天也很困 <105327571+like-monday@users.noreply.github.com>
2022-11-12 17:13:16 +0800

    功能全部完整

--- my_dx_admin/src/App.vue
+++ my_dx_admin/src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style>
+<style scoped>
+#app {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
 </style>
