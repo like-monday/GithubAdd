c3088db6eb91cfc1d193f56d6a6f9978a9adecef
今天也很困 <105327571+like-monday@users.noreply.github.com>
2022-11-12 17:08:35 +0800

    功能全部完整

--- my_dx_admin/src/views/Document/index.vue
+++ my_dx_admin/src/views/Document/index.vue
@@ -1,10 +1,41 @@
 <template>
   <div id="Document">
     <el-row>
-      <el-button type="primary" icon="el-icon-plus" @click="showDialog">添加</el-button>
+      <el-button type="primary" icon="el-icon-plus" @click="showDialog"
+        >添加</el-button
+      >
+      <el-select
+        class="select"
+        v-model="Modulevalue"
+        @change="getSelect"
+        placeholder="请选择模块"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        >
+        </el-option>
+      </el-select>
     </el-row>
-    <el-table :data="documentData" style="width: 100%" fit border>
-      <el-table-column align="center" prop="prop" label="序号" type="index" width="60">
+
+    <el-table
+      v-loading="loading"
+      element-loading-text="拼命加载中"
+      element-loading-spinner="el-icon-loading"
+      element-loading-background="rgba(0, 0, 0, 0.8)"
+      :data="documentData"
+      style="width: 100%"
+      border
+    >
+      <el-table-column
+        align="center"
+        prop="prop"
+        label="序号"
+        type="index"
+        width="60"
+      >
       </el-table-column>
       <el-table-column prop="name" label="网站名称" width="120">
       </el-table-column>
@@ -17,21 +48,48 @@
           <span v-show="row.icon == ''">此数据暂无图片......</span>
         </template>
       </el-table-column>
-      <el-table-column align="center" prop="lastdate" label="添加日期" width="180"></el-table-column>
+      <el-table-column
+        align="center"
+        prop="lastdate"
+        label="添加日期"
+        width="180"
+      ></el-table-column>
       <el-table-column align="center" prop="https" label="操作">
-        <template slot-scope="{ row, $index }">
-          <el-button size="mini" type="warning" icon="el-icon-edit" @click="updateTradeMark(row)">修改</el-button>
-          <el-button size="mini" type="danger" icon="el-icon-delete" @click="deleteDocuments(row)">删除</el-button>
+        <template slot-scope="{ row }">
+          <el-button
+            size="mini"
+            type="warning"
+            icon="el-icon-edit"
+            @click="updateTradeMark(row)"
+            >修改</el-button
+          >
+          <el-button
+            size="mini"
+            type="danger"
+            icon="el-icon-delete"
+            @click="deleteDocuments(row)"
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
-    <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
-      :current-page="search_form.page" :page-sizes="[5, 10, 15, 20]" :page-size="search_form.pagesize"
-      layout="total, sizes, prev, pager, next, jumper" :total="total">
+    <el-pagination
+      style="margin-top: 20px"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="search_form.page"
+      :page-sizes="[5, 10, 15, 20]"
+      :page-size="search_form.pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+    >
     </el-pagination>
 
     <!-- 弹出框 -->
-    <el-dialog :title="tmForm.id ? '修改文档资源' : '添加文档资源'" :visible.sync="dialogFormVisible">
+    <el-dialog
+      :title="tmForm.id ? '修改文档资源' : '添加文档资源'"
+      :visible.sync="dialogFormVisible"
+    >
       <!-- form表单 model属性：收集表单的数据到哪个对象身上-->
       <el-form style="width: 80%" :model="tmForm" ref="ruleForm">
         <el-form-item label="网站名称" label-width="120px">
@@ -45,7 +103,11 @@
            action:设置图片上传的地址
            :on-success：可以检测到图片上传成功，当图片上传成功，会执行一次
            :before-upload：可以在上传图片之前，会执行一次-->
-          <el-image style="width: 100px; height: 100px" :src="tmForm.icon" fit="cover"></el-image>
+          <el-image
+            style="width: 100px; height: 100px"
+            :src="tmForm.icon"
+            fit="cover"
+          ></el-image>
         </el-form-item>
         <el-form-item label="网站Logo地址" label-width="120px">
           <el-input autocomplete="off" v-model="tmForm.icon"></el-input>
@@ -59,7 +121,9 @@
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button v-if="tmForm.id" type="primary" @click="addOrUpdateDocument">修 改</el-button>
+        <el-button v-if="tmForm.id" type="primary" @click="addOrUpdateDocument"
+          >修 改</el-button
+        >
         <el-button v-else type="primary" @click="addDocuments">添 加</el-button>
       </div>
     </el-dialog>
@@ -73,21 +137,41 @@ import {
   addDocument,
   deleteDocument,
 } from "@/api/Document";
-import axios from 'axios'
 export default {
   data () {
     return {
+      options: [{
+        value: 'commonTools',
+        label: '常用工具'
+      }, {
+        value: 'technology',
+        label: '技术社区'
+      }, {
+        value: '选项3',
+        label: '日常摸鱼'
+      }, {
+        value: 'video',
+        label: '视频专区'
+      }, {
+        value: '选项5',
+        label: '练习专区'
+      }, {
+        value: 'document',
+        label: '原始数据'
+      }],
+      Modulevalue: 'document',
       total: 100, // 总条数
       search_form: {
         pagesize: 5, // 每页展示条数
         page: 1, // 当前页数
-        type: 'document'
+        type: 'technology'
       },
+      loading: true,
       documentData: [],
       // 弹出对话框收集的内容
       tmForm: {
         name: "",
-        describes:'',
+        describes: '',
         https: "",
         icon: "",
         lastdate: "",
@@ -113,11 +197,17 @@ export default {
     },
     //获取信息列表
     getDocument () {
-      // console.log('发送的数据：',JSON.stringify(this.search_form))
+      this.search_form.type = this.Modulevalue
+      this.loading = true
       reqDocument(this.search_form).then((result) => {
-        console.log("修改后的获取信息列表", result);
+        // console.log("修改后的获取信息列表", result);
+        if (result.data.total > 0 && result.data.data.length < 1) {
+          this.search_form.page += -1
+          this.getDocument()
+        }
         this.total = result.data.total
         this.documentData = result.data.data;
+        this.loading = false
       })
     },
     // 点击添加按钮
@@ -125,12 +215,12 @@ export default {
       this.dialogFormVisible = true;
       this.tmForm = {
         name: "",
-        describes:'',
+        describes: '',
         https: "",
         icon: "",
         lastdate: this.$formateDate("YYYY-mm-dd HH:MM", new Date()),
         id: "",
-        type: "document"
+        type: this.Modulevalue
       }
     },
     // 修改按钮
@@ -155,8 +245,15 @@ export default {
             message: result.data.message,
             type: "success",
           });
+          reqDocument(this.search_form).then((result) => {
+            if (result.data.total > 0 && result.data.data.length < 1) {
+              this.search_form.page += -1
+              this.getDocument()
+            } else {
+              this.getDocument();
+            }
+          })
           this.dialogFormVisible = false;
-          this.getDocument();
         } else {
           this.$message.error("删除失败");
         }
@@ -192,10 +289,13 @@ export default {
             type: "success",
           });
           this.getDocument();
-        }else{
+        } else {
           this.$message.error(result.data.message);
         }
       })
+    },
+    getSelect () {
+      this.getDocument()
     }
   },
 };
@@ -211,7 +311,7 @@ export default {
 }
 
 .avatar-uploader .el-upload:hover {
-  border-color: #409EFF;
+  border-color: #409eff;
 }
 
 .avatar-uploader-icon {
@@ -230,11 +330,16 @@ export default {
 }
 
 #Document {
+  box-sizing: border-box;
   padding: 20px;
 }
 
 .el-row {
   margin-bottom: 10px;
+}
+
+.select {
+  float: right;
 }
 
 .el-pagination {
