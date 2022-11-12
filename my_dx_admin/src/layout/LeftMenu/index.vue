90768e60e3e08727172c15ee4d503490d507fd5f
今天也很困 <105327571+like-monday@users.noreply.github.com>
2022-11-12 17:04:09 +0800

    功能全部完整

--- my_dx_admin/src/layout/LeftMenu/index.vue
+++ my_dx_admin/src/layout/LeftMenu/index.vue
@@ -1,58 +1,53 @@
 <template>
   <div class="leftMenu">
-    <div class="title">
-      <span></span>
-      <span>DX后台管理系统</span>
-    </div>
     <el-menu
-      default-active="1-4-1"
+      :default-active="this.$route.path"
       class="el-menu-vertical-demo"
       @open="handleOpen"
       @close="handleClose"
       text-color="rgb(191, 203, 217)"
       background-color="#304156"
       :collapse="isCollapse"
+      router
+      @select="handleSelect"
     >
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span slot="title">导航一</span>
-        </template>
-        <el-menu-item-group>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-      </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
-      </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
-      </el-menu-item>
+      <div v-if="!isCollapse" class="title">
+        <span></span>
+        <span>DX后台管理系统</span>
+      </div>
+      <div v-for="(item, index) in routerInfo" :key="index">
+        <el-menu-item :index="item.path">
+          <i :class="item.meta.icon"></i>
+          <span slot="title">{{ item.meta.title }}</span>
+        </el-menu-item>
+      </div>
     </el-menu>
   </div>
 </template>
 
 <script>
+import routerlist from '../../router/index'
 export default {
   data () {
     return {
-      isCollapse: false
     };
   },
+  props: ['isCollapse'],
+  created () {
+    this.getRouterInfo()
+  },
   methods: {
+    // 获取路由字段信息
+    getRouterInfo () {
+      this.routerInfo = routerlist.options.routes[0].children
+    },
     handleOpen (key, keyPath) {
-      console.log(key, keyPath);
     },
     handleClose (key, keyPath) {
-      console.log(key, keyPath);
-    }
+    },
+    // 接收到路由路径
+    handleSelect (key, keyPath) {
+    },
   }
 }
 </script>
@@ -63,18 +58,26 @@ export default {
   height: 100vh;
   background-color: #304156;
 }
+.el-menu--collapse {
+  height: 100vh;
+}
 </style>
 
 <style scoped lang='less'>
 .leftMenu {
-  width: 210px;
-  height: 100vh;
-  background-color: #304156;
+  //   width: 210px;
+  //   height: 100vh;
+  //   background-color: #304156;
   overflow: hidden;
   .title {
-    width: 100%;
+    width: 210px;
     text-align: center;
     padding: 20px 0;
+    // color: #0077f7;
+    color: #fff;
+    font-weight: 600;
+    font-size: 22px;
+    background-color: #293a4f;
   }
 }
 </style>
