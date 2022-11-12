b7759befb0b4e41c250c1093d47224d058d0c540
今天也很困 <105327571+like-monday@users.noreply.github.com>
2022-11-12 17:05:43 +0800

    功能全部完整

--- my_dx_admin/src/router/index.js
+++ my_dx_admin/src/router/index.js
@@ -11,27 +11,60 @@ const router = new Router({
             path: '/',
             name: 'layout',
             component: () => import('../layout/index.vue'),
-            // redirect: '/Main/Home',
-            meta: {
-                index: 3,
-            },
+            redirect: '/Home',
+            children: [
+                {
+                    path: '/Home',
+                    name: 'Home',
+                    component: () => import('../views/Document/index.vue'),
+                    meta: {
+                        index: "1",
+                        jurisdiction: true,
+                        title: '首页文档数据',
+                        icon: 'el-icon-s-order'
+                    }
+                },
+                {
+                    path: '/WebSever',
+                    name: 'WebSever',
+                    component: () => import('../views/WebSever/index.vue'),
+                    meta: {
+                        index: '2',
+                        jurisdiction: true,
+                        title: '前端文档数据',
+                        icon: 'el-icon-collection'
+                    }
+                },
+                {
+                    path: '/Journal',
+                    name: 'Journal',
+                    component: () => import('../views/Document/index.vue'),
+                    meta: {
+                        index: '3',
+                        jurisdiction: true,
+                        title: '迭代日志数据',
+                        icon: 'el-icon-table-lamp'
+                    }
+                },
+                {
+                    path: '/Role',
+                    name: 'Role',
+                    component: () => import('../views/Document/index.vue'),
+                    meta: {
+                        index: '4',
+                        jurisdiction: true,
+                        title: '角色设定数据',
+                        icon: 'el-icon-user-solid'
+                    }
+                }
+            ]
         },
-        // {
-        //     path: '/Main', // 骨架
-        //     name: 'pages',
-        //     component: () => import('@/pages/index.vue'),
-        //     redirect: '/Main/Home',
-        //     children: [
-        //         {
-        //             path: 'Home',
-        //             name: 'Home',
-        //             component: () => import('@/views/Home'),
-        //             meta: {
-        //                 index: 2,
-        //             },
-        //         }
-        //     ]
-        // },
+        {
+            path: 'web',
+            name: 'web',
+            component: () => import('../views/Document/index.vue')
+        }
+
     ]
 })
 
